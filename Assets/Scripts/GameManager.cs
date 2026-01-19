e505c872228d1da3bbce08e0a0302a5a28514b93
EricPont <eric.pont@estudiant.enti.cat>
2026-01-19 22:40:13 +0100

    fix gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,9 +10,7 @@ public class GameManager : MonoBehaviour
 
     private bool didGameEnded = false;
     private int[] dirValues = new int[4]; // 0 = N, 1 = S, 2 = E, 3 = W
-    private char[] playerDir = new char[4]; // 0 = p1, 1 = p2, 2 = p3, 3 = p4
 
-    private List<GameObject> loserPlayers = new List<GameObject>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -28,55 +26,55 @@ public class GameManager : MonoBehaviour
             //Fase dados = direcionts values
             dirValues = RandomDirections.Instance.RegenerateDirections();
 
-            //Fase eleccion dir de cada jugador
+            //Fase eleccion dir de cada jugador (inputs: lista de player, dir bloquada )
 
 
 
-            int countS = 0; int countN = 0; int countE = 0; int countW = 0;
+            //int countS = 0; int countN = 0; int countE = 0; int countW = 0;
 
-            char[] chars = { playerDir[0], playerDir[1], playerDir[2], playerDir[3] };
+            //char[] chars = { playerDir[0], playerDir[1], playerDir[2], playerDir[3] };
 
-            foreach (char c in chars)
-            {
-                switch (char.ToLower(c))
-                {
-                    case 's': countS++; break;
-                    case 'n': countN++; break;
-                    case 'e': countE++; break;
-                    case 'w': countW++; break;
-                }
-            }
+            //foreach (char c in chars)
+            //{
+            //    switch (char.ToLower(c))
+            //    {
+            //        case 's': countS++; break;
+            //        case 'n': countN++; break;
+            //        case 'e': countE++; break;
+            //        case 'w': countW++; break;
+            //    }
+            //}
 
-            if (countN > dirValues[0])
-            {
-                //Fase batalla con jugadores con playerDir = n
-
-
-
-            }
-            if (countS > dirValues[1])
-            {
-                //Fase batalla con jugadores con playerDir = s
+            //if (countN > dirValues[0])
+            //{
+            //    //Fase batalla con jugadores con playerDir = n (inputs: lista de player / output: player o players que perdieron)
 
 
 
-            }
-            if (countE > dirValues[2])
-            {
-                //Fase batalla con jugadores con playerDir = E
+            //}
+            //if (countS > dirValues[1])
+            //{
+            //    //Fase batalla con jugadores con playerDir = s (inputs: lista de player / output: player o players que perdieron)
 
 
 
-            }
-            if (countW > dirValues[3])
-            {
-                //Fase batalla con jugadores con playerDir = W
+            //}
+            //if (countE > dirValues[2])
+            //{
+            //    //Fase batalla con jugadores con playerDir = E (inputs: lista de player / output: player o players que perdieron)
 
 
 
-            }
+            //}
+            //if (countW > dirValues[3])
+            //{
+            //    //Fase batalla con jugadores con playerDir = W (inputs: lista de player / output: player o players que perdieron)
 
-            //Fase eleccion dir de jugador/es que perdieron
+
+
+            //}
+
+            //Fase eleccion dir de jugador/es que perdieron (inputs: lista de player, dir bloqueda)
 
 
 
@@ -89,14 +87,7 @@ public class GameManager : MonoBehaviour
 
 
 
-
-            //Fase votación
-
-
-
-
-            //Si empate en votación, batalla
-
+            //Fase votación (inputs: lista de player / output: player que ha perdido) 
 
 
 
